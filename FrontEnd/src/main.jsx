91d28f26f10cce8627e344070f80fc5f053158e4
RubensPaes <rubenspaesneto@gmail.com>
2025-10-16 12:30:52 -0400

    feita a pagina Home
    realizada a pagina Home no FrontEnd

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Home from './pages/Home1.jsx';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>    
+    <Routes>
+  <Route path="/" element={<Home/>} />
+    </Routes>
+     </BrowserRouter>
   </StrictMode>,
 )
---
+++ FrontEnd/src/pages/Home1.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "../css/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h1>ReservaFácil 🍽️</h1>
+      <p>Bem-vindo ao seu sistema de reserva de mesas!</p>
+
+      <div className="buttons">
+        <button>Login</button>
+        <button>Cadastro</button>
+        <button>Perfil</button>
+        <button>Atualizar Perfil</button>
+        <button>Cadastrar Mesas</button>
+        <button>Buscar Mesas</button>
+        <button>Reservar Mesas</button>
+        <button>Minhas Reservas</button>
+      </div>
+    </div>
+  );
+
+}
